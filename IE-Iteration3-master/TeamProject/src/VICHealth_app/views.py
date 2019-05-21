25e42a9dca7762b61ca8e961bcdba4a0c9906901
Finlay <finlay@dyn-118-139-70-14.its.monash.edu.au>
2019-05-21 20:29:39 +1000

    Top

---
+++ IE-Iteration3-master/TeamProject/src/VICHealth_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, render_to_response, get_object_or_404, redirect
+from .models import Club
+from .forms import InputForm
+
+# Create your views here.
+def base(request):
+    return render(request, 'VICHealth_app/base.html')
+
+def index(request):
+    return render(request, 'VICHealth_app/index.html')
+
+def check_activity_level(request):
+    return render(request, 'VICHealth_app/check_activity_level.html')
+
+def health_tips(request):
+    return render(request, 'VICHealth_app/health_tips.html')
+
+def data(request):
+    return render(request, 'VICHealth_app/data.html')
+
+def diet(request):
+    return render(request, 'VICHealth_app/diet.html')
+
+def gg(request):
+    return render(request, 'VICHealth_app/google4190dd0082df8a9e.html')
+
+def about_us(request):
+    return render(request, 'VICHealth_app/about_us.html')
+
+def sub_info(request):
+    club=Club.objects.all()
+    form=InputForm()
+
+
+    context = { "club":club, "form":form }
+    return render(request, 'VICHealth_app/sub_info.html', context)
